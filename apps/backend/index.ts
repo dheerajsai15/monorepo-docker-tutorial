81105696a580156efd873b73696603b9d5bff259
Dheeraj Sai <dheerajsaivnr@gmail.com>
2025-11-22 23:08:42 +0530

    Completed application logic

---
+++ apps/backend/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import { prismaClient } from "db/client";
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/users",async (req, res) => {
+    const users = await prismaClient.user.findMany();
+    res.json({
+        users
+    })
+})
+
+app.post("/user",async (req,res) => {
+    const { username, password } = req.body;
+
+    if(!username || !password){
+        return res.status(400).json({
+            error: "Username and password are required"
+        })
+    }
+
+    const user = await prismaClient.user.create({
+        data: {
+            username,
+            password
+        }
+    });
+
+    res.json({
+        user,
+        message: "User created Successfully!"
+    })
+})
+
+app.listen(8080);
